0a68521fa3f0bebba455de2fb8af396de784f2dc
Bikash12tri <bikashtripathy646@gmail.com>
2022-08-17 12:48:17 +0530

    demo

---
+++ src/controller/myBookController.js
@@ -0,0 +1,42 @@
+const { find } = require("../models/bookModel")
+const BookModel= require("../models/bookModel")
+//------------------------------------------------1---------------------------------------------//
+const createBook= async function (req, res) {
+    let data = req.body
+    let savedData = await BookModel.create(data)
+    res.send({msg: savedData})
+}
+//------------------------------------------------2--------------------------------------------//
+const booksList = async function (req, res) {
+    let allBooks= await BookModel.find().select({  _id : 0, bookName : 1 , authorName : 1 })
+    res.send({msg: allBooks})
+}
+//------------------------------------------------3-------------------------------------------//
+const getBooksInYear = async function (req, res) {
+    let years = req.params.year
+    let allBooks= await BookModel.find({year: { $eq: years}})
+    res.send({msg: allBooks})
+}
+//------------------------------------------------4----------------------------------------------//
+const getParticularBooks = async function (req, res) {
+    let obj = req.body
+    let allBooks= await BookModel.find(meow)
+    res.send({msg: allBooks})
+}
+//------------------------------------------------5---------------------------------------------//
+const getXINRBooks = async function (req, res) {
+    let allBooks= await BookModel.find({ "prices.indianPrice" : {$in : ["100INR","200INR","500INR"]} })
+    res.send({msg: allBooks})
+}
+//------------------------------------------------6--------------------------------------------//
+const getRandomBooks  = async function (req, res) {
+    let allBooks= await BookModel.find({$or: [{stockAvailable: true},{totalPages:{$gt: 500}}]})
+    res.send({msg: allBooks})
+}
+
+module.exports.createBook= createBook
+module.exports.booksList= booksList
+module.exports.getBooksInYear = getBooksInYear
+module.exports.getParticularBooks = getParticularBooks
+module.exports.getXINRBooks = getXINRBooks
+module.exports.getRandomBooks = getRandomBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,190 +1,205 @@
-const express = require('express');
-const router = express.Router();
-const UserController= require("../controllers/bookController")
+// const express = require('express');
+// const router = express.Router();
+// const UserController= requicre("../controllers/UserController")
+// const bookController= require("../controllers/bookController")
+// const bookController= require("../controllers/myBookController")
 
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+// router.get('/students/:name', function(req, res) {
+//     let studentName = req.params.name
+//     console.log(studentName)
+//     res.send(studentName)
+// })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
-
-
+// router.get("/random" , function(req, res) {
+//     res.send("hi there")
+// })
 
 
 
 
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
 
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
+// router.get("/test-api" , function(req, res) {
+//     res.send("hi FunctionUp")
+// })
 
 
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
+// router.get("/test-api-2" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API")
+// })
 
 
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-//1st Assignment
-let players =
-   [
-       {
-           "name": "manish",
-           "dob": "1/1/1995",
-           "gender": "male",
-           "city": "jalandhar",
-           "sports": [
-               "swimming"
-           ]
-       },
-       {
-           "name": "gopal",
-           "dob": "1/09/1995",
-           "gender": "male",
-           "city": "delhi",
-           "sports": [
-               "soccer"
-           ]
-       },
-       {
-           "name": "lokesh",
-           "dob": "1/1/1990",
-           "gender": "male",
-           "city": "mumbai",
-           "sports": ["soccer"
-        ]
-    },
-]
+// router.get("/test-api-3" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
+// })
 
 
-router.post('/players', function (req, res) {
+// router.get("/test-api-4" , function(req, res) {
+//     res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+// })
+
+// //1st Assignment
+// let players =
+//    [
+//        {
+//            "name": "manish",
+//            "dob": "1/1/1995",
+//            "gender": "male",
+//            "city": "jalandhar",
+//            "sports": [
+//                "swimming"
+//            ]
+//        },
+//        {
+//            "name": "gopal",
+//            "dob": "1/09/1995",
+//            "gender": "male",
+//            "city": "delhi",
+//            "sports": [
+//                "soccer"
+//            ]
+//        },
+//        {
+//            "name": "lokesh",
+//            "dob": "1/1/1990",
+//            "gender": "male",
+//            "city": "mumbai",
+//            "sports": ["soccer"
+//         ]
+//     },
+// ]
+
+
+// router.post('/players', function (req, res) {
     
-    let newPlayer = req.body
-    let newPlayersName = newPlayer.name
-    let repeatedPerson = false
+//     let newPlayer = req.body
+//     let newPlayersName = newPlayer.name
+//     let repeatedPerson = false
 
-    for(let i = 0; i < players.length; i++) {
-        if(players[i].name == newPlayersName) {
-            repeatedPerson = true;
-            break;
-        }
-    }
-    if (repeatedPerson == true) {
-        res.send("This player was already exsist")
-    } else {
-        players.push(newPlayer)
-        res.send(players)
-    }
-});
+//     for(let i = 0; i < players.length; i++) {
+//         if(players[i].name == newPlayersName) {
+//             repeatedPerson = true;
+//             break;
+//         }
+//     }
+//     if (repeatedPerson == true) {
+//         res.send("This player was already exsist")
+//     } else {
+//         players.push(newPlayer)
+//         res.send(players)
+//     }
+// });
 
-//2 nd Assignment
-let booking = [
-    {
-        bookingNumber: 1,
-        bookingId: 12,
-        sportId: "",
-        centerId: "",
-        type: "private",
-        slot: "16286598000000",
-        bookedOn: "31/08/2021",
-        bookedFor: "01/09/2021",
-    },
-];
+// //2 nd Assignment
+// let booking = [
+//     {
+//         bookingNumber: 1,
+//         bookingId: 12,
+//         sportId: "",
+//         centerId: "",
+//         type: "private",
+//         slot: "16286598000000",
+//         bookedOn: "31/08/2021",
+//         bookedFor: "01/09/2021",
+//     },
+// ];
 
 
-router.post("/players/:playerName/bookings/:bookingId", function (req, res) {
-    let playerExist = false
-    for (let i = 0; i < players.length; i++) {
-        if (players[i].name == req.params.playerName) {
-            playerExist = true
-        }
-    }
-    if (!playerExist) {
-        return res.send("This player does not exist")
-    }
-    for (let i = 0; i < booking.length; i++) {
-        if ((booking[i].bookingId == req.params.bookingId)) {
-            return res.send("This booking id already existed in Data");
-        }
-    }
-    req.body.playerName = req.params.playerName
-    req.body.bookingId = req.params.bookingId
+// router.post("/players/:playerName/bookings/:bookingId", function (req, res) {
+//     let playerExist = false
+//     for (let i = 0; i < players.length; i++) {
+//         if (players[i].name == req.params.playerName) {
+//             playerExist = true
+//         }
+//     }
+//     if (!playerExist) {
+//         return res.send("This player does not exist")
+//     }
+//     for (let i = 0; i < booking.length; i++) {
+//         if ((booking[i].bookingId == req.params.bookingId)) {
+//             return res.send("This booking id already existed in Data");
+//         }
+//     }
+//     req.body.playerName = req.params.playerName
+//     req.body.bookingId = req.params.bookingId
 
-    booking.push(req.body);
-    return res.send(booking);
-});
+//     booking.push(req.body);
+//     return res.send(booking);
+// });
 
-//3rd Assignment
+// //3rd Assignment
 
-let persons = [
-    {
-      name : "PK",
-      age : 10,
-      votingstatus : false
-    },
-    {
-        name : "Sk",
-        age : 20,
-        votingstatus : false
-    },
-    {
-        name : "AA",
-        age : 70,
-        votingstatus : false
-    },
-    {
-        name : "SC",
-        age : 5,
-        votingstatus : false
-    },
-    {
-        name : "HQ",
-        age : 40,
-        votingstatus : false
-    }
-]
-router.post("/persons", function (req,res){
+// let persons = [
+//     {
+//       name : "PK",
+//       age : 10,
+//       votingstatus : false
+//     },
+//     {
+//         name : "Sk",
+//         age : 20,
+//         votingstatus : false
+//     },
+//     {
+//         name : "AA",
+//         age : 70,
+//         votingstatus : false
+//     },
+//     {
+//         name : "SC",
+//         age : 5,
+//         votingstatus : false
+//     },
+//     {
+//         name : "HQ",
+//         age : 40,
+//         votingstatus : false
+//     }
+// ]
+// router.post("/persons", function (req,res){
 
-let votingAge = req.query.votingAge
-    let result = []
-    for(let i =0; i<persons.length;i++){
-        let id = persons[i]
-        if(id.age>votingAge){
-            id.votingstatus=true
-             result.push(id)
-        }
-    }
+// let votingAge = req.query.votingAge
+//     let result = []
+//     for(let i =0; i<persons.length;i++){
+//         let id = persons[i]
+//         if(id.age>votingAge){
+//             id.votingstatus=true
+//              result.push(id)
+//         }
+//     }
     
-    return res.send({data : result , status : true})
+//     return res.send({data : result , status : true})
 
-});
+// });
 
 
 // const express = require('express');
 // // const router = express.Router();
 // const UserController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
 
-router.post("/createBooks", UserController.createBooks  )
+// router.post("/createBooks", UserController.createBooks  )
 
-router.get("/getBooks", UserController.getBooks)
+// router.get("/getBooks", UserController.getBooks)
 
-module.exports = router;
+
+router.post("/createBook", myBookController.createBook  )
+
+router.get("/bookList", myBookController.booksList)
+
+router.post("/getBooksInYear/:year", myBookController.getBooksInYear)
+
+router.post("/getParticularBooks", myBookController.getParticularBooks)
+
+router.get("/getXINRBooks", myBookController.getXINRBooks)
+
+router.get("/getRandomBooks", myBookController.getRandomBooks)
+
+
 
  module.exports = router;
