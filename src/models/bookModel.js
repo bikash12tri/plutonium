ece4535954a18ee84425619518559089835dca99
Bikash12tri <bikashtripathy646@gmail.com>
2022-08-19 13:10:05 +0530

    demo

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,27 +1,27 @@
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const bookSchema = new mongoose.Schema( {
-//     bookName: {
-//         type : String,
-//         required : true
-//     },
-//     authorName: String, 
-//     tags: [String],
-//     year : {
-//         type : Number,
-//         default : 2021
-//     },
-//     isPublished: Boolean,
-//     prices: {
-//         indianPrice: String,
-//         europePrice: String,
-//     },
-//     totalPages : Number,
-//     stockAvailable : Boolean
-// }, { timestamps: true });
-
-
+const bookSchema = new mongoose.Schema( {
+    bookName: {
+        type : String,
+        required : true
+    },
+    authorName: String, 
+    tags: [String],
+    year : {
+        type : Number,
+        default : 2021
+    },
+    isPublished: Boolean,
+    prices: {
+        indianPrice: String,
+        europePrice: String,
+    },
+    totalPages : Number,
+    stockAvailable : Boolean
+ }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema)
+
+
+ module.exports = mongoose.model('Author', AuthorSchema)
